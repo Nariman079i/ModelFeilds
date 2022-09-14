946765f632c5b0b66a14e322210b82b9882cbd0b
Nariman <Nariman079i>
2022-09-14 19:20:44 +0300

    3

--- ModelFields/main/views.py
+++ ModelFields/main/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from .models import *
+
+
 # Create your views here.
+
 def index(requests):
-    model =
+    int_post = IntFields()
+    file = 'main/index.html'
+    context = {
+        'post': int_post
+    }
+    return render(requests, file, context=context)
